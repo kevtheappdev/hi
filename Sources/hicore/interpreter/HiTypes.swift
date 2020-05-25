a2e1aa3365f9a7f3204bb5bb0707b5fceb9b82cc
Kevin Turner <kevinturner@utexas.edu>
2020-05-24 19:26:02 -0500

    Adds license, prototype new type object.

--- Sources/hicore/interpreter/HiTypes.swift
+++ Sources/hicore/interpreter/HiTypes.swift
@@ -1,6 +1,6 @@
 //
 //  HiType.swift
-//  
+//  Prototype universal object model
 //
 //  Created by Kevin Turner on 5/24/20.
 //
@@ -8,22 +8,38 @@
 import Foundation
 
 
-class HiType: Comparable {
-    static func < (lhs: HiType, rhs: HiType) -> Bool {
-        return false
-    }
+class HiType: CustomStringConvertible {
     
-    static func == (lhs: HiType, rhs: HiType) -> Bool {
-        return lhs === rhs
+    var description: String {
+        get {
+            return "nada"
+        }
     }
-
 }
 
 class HiNumber: HiType {
+    override var description: String {
+        get {
+            let str = String(describing: self.value)
+            if str.hasSuffix(".0"), let index = str.lastIndex(of: ".") {
+                return String(str[str.startIndex..<index])
+            }
+            return str
+        }
+    }
+    
     var value: Double
+    
     
     init(value: Double) {
         self.value = value
     }
     
+    static func + (lhs: HiNumber, rhs: HiNumber) -> HiNumber {
+        return HiNumber(value: lhs.value + rhs.value)
+    }
+    
+    static func * (lhs: HiNumber, rhs: HiNumber) -> HiNumber {
+        return HiNumber(value: lhs.value * rhs.value)
+    }
 }
